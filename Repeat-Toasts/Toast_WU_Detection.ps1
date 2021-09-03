cb741fa3a4edea44f132ef718876245d3cca2df9
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-03 09:08:33 +0100

    added

---
+++ Repeat-Toasts/Toast_WU_Detection.ps1
@@ -0,0 +1,25 @@
+$Path = "HKLM:\Software\!ProactiveRemediations"
+$Name = "20H2NotificationSchTaskCreated"
+$Value = 1
+
+$TargetDate = (Get-Date -Day 24 -Month 5 -Year 2021).ToString("ddMMyyy")
+$ClientDate = (Get-Date).ToString("ddMMyyy")
+
+If (!($TargetDate -eq $ClientDate)){
+    Write-Output "Remediation Target Date""$($TargetDate)"" not valid. Client date is $ClientDate. Remediation will not run."
+    Exit 0
+}
+
+Try {
+    $Registry = Get-ItemProperty -Path $Path -Name $Name | Select-Object -ExpandProperty $Name
+    If ($Registry -eq $Value){
+        Write-Output "Remediation not required."
+        Exit 0
+    } 
+    Write-Output "Remediation required"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Error Caught. Remediation will not run."
+    Exit 0
+}
