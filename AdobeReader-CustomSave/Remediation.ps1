a863cec2bf955fb9d1d71889eb0f19106385cce4
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-30 10:23:14 +0100

    Update Remediation.ps1

--- AdobeReader-CustomSave/Remediation.ps1
+++ AdobeReader-CustomSave/Remediation.ps1
@@ -1,3 +1,18 @@
+<# 
+.SYNOPSIS
+   Fix Blank SaveAs screen in Adobe Acrobat Reader DC.
+
+.DESCRIPTION
+   Creates registry entry to untick "Show online storage when saving files".
+
+.EXAMPLE
+   PS C:\> .\Remediation.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v4+
+#>
+
 $Path = "HKCU:\Software\Adobe\Adobe Acrobat\DC\AVGeneral\"
 $Name = "bToggleCustomSaveExperience"
 $Type = "DWORD"
