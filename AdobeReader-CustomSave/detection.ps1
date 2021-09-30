05ce0eceef5101d2764ca4c7b7ff3fe5c421d521
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-30 10:18:30 +0100

    added to repo

---
+++ AdobeReader-CustomSave/detection.ps1
@@ -0,0 +1,18 @@
+$Path = "HKCU:\Software\Adobe\Acrobat Reader\DC\AVGeneral"
+$Name = "bToggleCustomSaveExperience"
+$Type = "DWORD"
+$Value = 0
+
+Try {
+    $Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop | Select-Object -ExpandProperty $Name
+    If ($Registry -eq $Value){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    Write-Warning "Not Compliant"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
