cef5c39c96df46dfe24cae5965cc9cfe4d22a190
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-30 10:21:34 +0100

    Update detection.ps1

--- AdobeReader-CustomSave/detection.ps1
+++ AdobeReader-CustomSave/detection.ps1
@@ -1,3 +1,18 @@
+<# 
+.SYNOPSIS
+   Fix Blank SaveAs screen in Adobe Acrobat Reader DC.
+
+.DESCRIPTION
+   Detect if the "Show online storage when saving files" registry entry is present on a device .
+
+.EXAMPLE
+   PS C:\> .\Detection.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v1+
+#>
+
 $Path = "HKCU:\Software\Adobe\Acrobat Reader\DC\AVGeneral"
 $Name = "bToggleCustomSaveExperience"
 $Type = "DWORD"
