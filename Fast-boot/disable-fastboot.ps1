023c99d62ea987e9aa6d566ad48bd583a0003c56
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2021-10-28 10:47:31 +0100

    added to repo

---
+++ Fast-boot/disable-fastboot.ps1
@@ -0,0 +1,4 @@
+
+
+if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name 'HiberbootEnabled' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
