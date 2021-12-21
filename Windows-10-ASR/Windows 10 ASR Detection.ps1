298c5e3ac9766554dfeb599d288beac2da7ec205
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2021-12-21 11:29:02 +0000

    updated

--- Windows-10-ASR/Windows 10 ASR Detection.ps1
+++ Windows-10-ASR/Windows 10 ASR Detection.ps1
@@ -3,7 +3,6 @@
   Previous script was failing and was not correctly going into remediation
   Using script here: https://github.com/anthonws/MDATP_PoSh_Scripts/blob/master/ASR/ASR_Analyzer_v2.2.ps1
   Logic: If there are not 16 rules in total go straight into remediation
-  
   Script has been updated to create a logfile on the machine
   This logfile can then be pulled via Live response within the M365 Defender portal assuming it is on without resorting to Diagnostics
 #>
@@ -20,7 +19,6 @@ $RulesIdsArray = @()
 $RulesIdsArray += $RulesIds
 
 $counter = 0
-$Total = 0
 $TotalNotConfigured = 0
 $TotalAudit = 0
 $TotalBlock = 0
@@ -32,7 +30,7 @@ $LogName = "C:\Temp\ASR-Verification.Log"
 
 
 Function Tee-Log 
-    {
+    { #writes to a file the output that you designate
     param([Parameter(Mandatory = $true, ValueFromPipeline = $true)]
             $Input,
           [Parameter(Mandatory = $true)]
@@ -81,8 +79,9 @@ If (! (Test-Path $LogName))
 
 #write the following values to the log file as a seperation 
 
-'*********************************************************************' | Tee-Log -FilePath $LogName -Silent:$Passthru
-"$(get-date -format u)  :  INFO  : ComputerName: $($Env:ComputerName)" | Tee-Log -FilePath $LogName -Silent:$Passthru
+"========================================== DEVICE ========================================" | Tee-Log -FilePath $LogName -Silent:$Passthru
+"$(get-date -format u)  :  INFO  : ComputerName: $($Env:ComputerName) VERSION 4" | Tee-Log -FilePath $LogName -Silent:$Passthru
+"==========================================================================================" | Tee-Log -FilePath $LogName -Silent:$Passthru
 
 
 
@@ -111,22 +110,22 @@ $counter = 0
 ForEach ($j in $RulesIds)
     {
     ## Convert GUID into Rule Name
-     If ($RulesIdsArray[$counter] -eq "56a863a9-875e-4185-98a7-b882c64b5ce5"){$RuleName = "Block abuse of exploited vulnerable signed drivers"}
-    ElseIf ($RulesIdsArray[$counter] -eq "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c"){$RuleName = "Block Adobe Reader from creating child processes"}
-    ElseIf ($RulesIdsArray[$counter] -eq "D4F940AB-401B-4EFC-AADC-AD5F3C50688A"){$RuleName = "Block all Office applications from creating child processes"}
-    ElseIf ($RulesIdsArray[$counter] -eq "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2"){$RuleName = "Block credential stealing from the Windows local security authority subsystem (lsass.exe)"}
-    ElseIf ($RulesIdsArray[$counter] -eq "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550"){$RuleName = "Block executable content from email client and webmail"}
-    ElseIf ($RulesIdsArray[$counter] -eq "01443614-cd74-433a-b99e-2ecdc07bfc25"){$RuleName = "Block executable files from running unless they meet a prevalence, age, or trusted list criteria"}
-    ElseIf ($RulesIdsArray[$counter] -eq "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC"){$RuleName = "Block execution of potentially obfuscated scripts"}
-    ElseIf ($RulesIdsArray[$counter] -eq "D3E037E1-3EB8-44C8-A917-57927947596D"){$RuleName = "Block JavaScript or VBScript from launching downloaded executable content"}
-    ElseIf ($RulesIdsArray[$counter] -eq "3B576869-A4EC-4529-8536-B80A7769E899"){$RuleName = "Block Office applications from creating executable content"}
-    ElseIf ($RulesIdsArray[$counter] -eq "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84"){$RuleName = "Block Office applications from injecting code into other processes"}
-    ElseIf ($RulesIdsArray[$counter] -eq "26190899-1602-49e8-8b27-eb1d0a1ce869"){$RuleName = "Block Office communication applications from creating child processes"}
-    ElseIf ($RulesIdsArray[$counter] -eq "e6db77e5-3df2-4cf1-b95a-636979351e5b"){$RuleName = "Block persistence through WMI event subscription"}
-    ElseIf ($RulesIdsArray[$counter] -eq "d1e49aac-8f56-4280-b9ba-993a6d77406c"){$RuleName = "Block process creations originating from PSExec and WMI commands"}
-    ElseIf ($RulesIdsArray[$counter] -eq "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4"){$RuleName = "Block untrusted and unsigned processes that run from USB"}
-    ElseIf ($RulesIdsArray[$counter] -eq "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B"){$RuleName = "Block Win32 API calls from Office macro"}
-    ElseIf ($RulesIdsArray[$counter] -eq "c1db55ab-c21a-4637-bb3f-a12568109d35"){$RuleName = "Use advanced protection against ransomware"}
+     If ($RulesIdsArray[$counter] -eq "56a863a9-875e-4185-98a7-b882c64b5ce5"){$RuleName = "Abuse of exploited vulnerable signed drivers"}
+    ElseIf ($RulesIdsArray[$counter] -eq "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c"){$RuleName = "Adobe Reader from creating child processes"}
+    ElseIf ($RulesIdsArray[$counter] -eq "D4F940AB-401B-4EFC-AADC-AD5F3C50688A"){$RuleName = "Office applications from creating child processes"}
+    ElseIf ($RulesIdsArray[$counter] -eq "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2"){$RuleName = "Credential stealing from the Windows local security authority subsystem (lsass.exe)"}
+    ElseIf ($RulesIdsArray[$counter] -eq "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550"){$RuleName = "Executable content from email client and webmail"}
+    ElseIf ($RulesIdsArray[$counter] -eq "01443614-cd74-433a-b99e-2ecdc07bfc25"){$RuleName = "Executable files from running unless they meet a prevalence, age, or trusted list criteria"}
+    ElseIf ($RulesIdsArray[$counter] -eq "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC"){$RuleName = "Execution of potentially obfuscated scripts"}
+    ElseIf ($RulesIdsArray[$counter] -eq "D3E037E1-3EB8-44C8-A917-57927947596D"){$RuleName = "JavaScript or VBScript from launching downloaded executable content"}
+    ElseIf ($RulesIdsArray[$counter] -eq "3B576869-A4EC-4529-8536-B80A7769E899"){$RuleName = "Office applications from creating executable content"}
+    ElseIf ($RulesIdsArray[$counter] -eq "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84"){$RuleName = "Office applications from injecting code into other processes"}
+    ElseIf ($RulesIdsArray[$counter] -eq "26190899-1602-49e8-8b27-eb1d0a1ce869"){$RuleName = "Office communication applications from creating child processes"}
+    ElseIf ($RulesIdsArray[$counter] -eq "e6db77e5-3df2-4cf1-b95a-636979351e5b"){$RuleName = "Persistence through WMI event subscription"}
+    ElseIf ($RulesIdsArray[$counter] -eq "d1e49aac-8f56-4280-b9ba-993a6d77406c"){$RuleName = "Process creations originating from PSExec and WMI commands"}
+    ElseIf ($RulesIdsArray[$counter] -eq "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4"){$RuleName = "Untrusted and unsigned processes that run from USB"}
+    ElseIf ($RulesIdsArray[$counter] -eq "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B"){$RuleName = "Win32 API calls from Office macro"}
+    ElseIf ($RulesIdsArray[$counter] -eq "c1db55ab-c21a-4637-bb3f-a12568109d35"){$RuleName = "Use Advanced protection against ransomware"}
 
     ## Check the Action type
     If ($RulesActions[$counter] -eq 0){$RuleAction = "Disabled"}
@@ -135,7 +134,7 @@ ForEach ($j in $RulesIds)
 
     ## Output Rule Id, Name and Action
 
-    "=> $($RulesIdsArray[$counter])  ** $RuleName ** Action:$RuleAction " | Tee-Log -FilePath $LogName -Silent:$Passthru
+    "=> $($RulesIdsArray[$counter])  ** Action:$RuleAction --- $RuleName " | Tee-Log -FilePath $LogName -Silent:$Passthru
     $counter++
 
     }
--- Windows-10-ASR/Windows 10 ASR Remediation.ps1
+++ Windows-10-ASR/Windows 10 ASR Remediation.ps1
@@ -1,11 +1,11 @@
 <#
    Remediation for Windows 10 ASR Settings
    Three ways of doing this inside the test tenant
-    1.  Set the registry key which appears to be instantaneous
+    1.  Set the registry key which appears to be instantaneous although there are actually two - one of which is relevant to the portal
     2.  Use the Set-MpPreference command
-    3.  Use ADMX custom policy in MDM as Intune is already set to configure ASR
-    4.  Force a resync of the entire device - may be better, but not guaranteed
-    Updated 2021-07-28
+    3.  Use ADMX custom policy in MDM as Intune is already set to configure ASR (cant use require enterprise)
+    4.  Force a resync of the entire device - worst solution.  Obviously it isnt working
+    Updated 2021-12-18
 #>
 
 #SET VARIABLES
@@ -15,7 +15,23 @@
 
 try
     {
-    Get-ScheduledTask | Where-Object {$_.TaskName -eq ‘PushLaunch’} | Start-ScheduledTask
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 56a863a9-875e-4185-98a7-b882c64b5ce5 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids d4f940ab-401b-4efc-aadc-ad5f3c50688a -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 56a863a9-875e-4185-98a7-b882c64b5ce5 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids be9ba2d9-53ea-4cdc-84e5-9b1eeee46550 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 01443614-cd74-433a-b99e-2ecdc07bfc25 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 5beb7efe-fd9a-4556-801d-275e5ffc04cc -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids d3e037e1-3eb8-44c8-a917-57927947596d -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 3b576869-a4ec-4529-8536-b80a7769e899 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 75668c1f-73b5-4cf0-bb93-3ecf5cb7cc84 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 26190899-1602-49e8-8b27-eb1d0a1ce869 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids d1e49aac-8f56-4280-b9ba-993a6d77406c -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4 -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids 92e97fa1-2edf-4476-bdd6-9dd0b4dddc7b -AttackSurfaceReductionRules_Actions Enabled
+    Add-MpPreference -AttackSurfaceReductionRules_Ids c1db55ab-c21a-4637-bb3f-a12568109d35 -AttackSurfaceReductionRules_Actions Enabled
     exit 0
     }
 catch
